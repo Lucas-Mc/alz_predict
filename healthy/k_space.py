34d984e12194a9845c81e3e81943071b2c1e96b3
Lucas McCullum <lumccul3@gmail.com>
2020-09-17 14:16:12 -0400

    Finishes up all FTs and IFTs

--- healthy/k_space.py
+++ healthy/k_space.py
@@ -3,8 +3,9 @@
 # Used to convert MRI images to k-Space
 
 # Outside packages
-import pydicom
+import matplotlib.pyplot as plt
 import numpy as np
+import pydicom
 import cmath
 import pdb
 
@@ -13,7 +14,7 @@ normal_im = data.pixel_array
 
 # Perform the 2-Dimensional Discrete Fourier Transform (DFT) to get the k-Space
 # F[k,l] = \sum_{m=0}^{N-1}\sum_{n=0}^{N-1}f[m,n]e^{(-i)\frac{2\pi km}{N}}e^{(-i)\frac{2\pi ln}{N}}
-N = normal_im.shape[0]
+# N = normal_im.shape[0]
 
 # VERYYYY slow way
 # fourier_im = np.empty((N,N), dtype=np.complex_)
@@ -34,4 +35,40 @@ N = normal_im.shape[0]
 
 # Much faster way
 fourier_im = np.fft.fft2(normal_im)
-pdb.set_trace()
+inverse_fourier_im = np.fft.ifft2(fourier_im)
+
+# Plot the Fourier Transformation
+plt.figure(figsize=(10,4))
+plt.suptitle('Fourier Tranformation of Original Image')
+# Plot the real components
+plt.subplot(1,3,1)
+plt.title('Real')
+plt.imshow(fourier_im.real  > 0, cmap='Greys')
+# Plot the imaginary components
+plt.subplot(1,3,2)
+plt.title('Imaginary')
+plt.imshow(fourier_im.imag  > 0, cmap='Greys')
+# Plot the absolute value
+plt.subplot(1,3,3)
+plt.title('Absolute Value')
+plt.imshow(abs(fourier_im > 0), cmap='Greys')
+# Show the plot
+plt.show()
+
+# Plot the Inverse Fourier Transformation
+plt.figure(figsize=(10,4))
+plt.suptitle('Inverse Fourier Tranformation of Fourier Transform of Original Image')
+# Plot the real components
+plt.subplot(1,3,1)
+plt.title('Real')
+plt.imshow(inverse_fourier_im.real, cmap='Greys')
+# Plot the imaginary components
+plt.subplot(1,3,2)
+plt.title('Imaginary')
+plt.imshow(inverse_fourier_im.imag, cmap='Greys')
+# Plot the absolute value
+plt.subplot(1,3,3)
+plt.title('Absolute Value')
+plt.imshow(abs(inverse_fourier_im), cmap='Greys')
+# Show the plot
+plt.show()
